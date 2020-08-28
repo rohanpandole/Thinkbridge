1260de35e9d4c5560c8cdc5cfd097ba1b90cd7ba
rohanpandole <rohanpandole@gmail.com>
2020-08-28 21:10:57 +0530

    Added method for fraction value to word

--- src/TestCode.java
+++ src/TestCode.java
@@ -7,6 +7,11 @@ class TestCode
 
     static String tenMultiple[] = { "", "", "Twenty", "Thirty", "Forty","Fifty", "Sixty", "Seventy", "Eighty",
             "Ninety" };
+    static String oneNumber[] = { "0", "1/100 Only", "2/100 Only", "3/100 Only", "4/100 Only","5/100 Only", "6/100 Only", "7/100 Only", "8/100 Only",
+            "9/100 Only", "10/100 Only", "11/100 Only", "12/100 Only","13/100 Only", "14/100 Only", "15/100 Only","16/100 Only", "17/100 Only", "18/100 Only","19/100 Only" };
+
+
+    static String tenNumber[] = { "", "", "2", "3", "4","5", "6", "7", "8","9" };
 
     static String powerToWord(int n)
     {
@@ -32,6 +37,27 @@ class TestCode
             return powerToWord(n/1000)+" Thousand"+((n%1000!=0)?" ":"")+powerToWord(n%1000);
         }
         return powerToWord(n/100000)+" Lakh"+((n%100000!=0)?" ":"")+powerToWord(n%100000);
+    }
+    static String fractionToWord(int n)
+    {
+
+        if(n<20)
+        {
+            return oneNumber[n];
+        }
+        if (n<100)
+        {
+            return tenNumber[n/10]+fractionToWord(n%10);
+        }
+        if (n<1000)
+        {
+            return oneNumber[n/100]+fractionToWord(n%100);
+        }
+        if(n<100000)
+        {
+            return fractionToWord(n/1000)+fractionToWord(n%1000);
+        }
+        return fractionToWord(n/100000)+fractionToWord(n%100000);
     }
 
 }
