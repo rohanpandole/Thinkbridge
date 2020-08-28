233d55f41bd94a1b311441c46f4977ce10caebdf
rohanpandole <rohanpandole@gmail.com>
2020-08-28 13:13:53 +0530

    Got input value from consol

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,15 @@
-public class Test
+import java.util.Scanner;
+
+class Test
 {
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" Enter digit :");
 
+        long number = sc.nextLong();
+
+        System.out.println("Digit in word : ");
+        System.out.print(number);
     }
 }
