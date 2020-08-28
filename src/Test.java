c29806817b49f57a80cc613ae3e8d3384391d372
rohanpandole <rohanpandole@gmail.com>
2020-08-28 13:54:48 +0530

    Code accept 1 to 19 digit for conversion into word

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,36 @@ import java.util.Scanner;
 
 class Test
 {
+    static String oneMultiple[] = { "", "oneMultiple ", "two ", "three ", "four ","five ", "six ", "seven ", "eight ",
+            "nine ", "tenMultiple ", "eleven ", "twelve ","thirteen ", "fourteen ", "fifteen ",
+            "sixteen ", "seventeen ", "eighteen ","nineteen " };
+
+
+    static String tenMultiple[] = { "", "", "twenty ", "thirty ", "forty ","fifty ", "sixty ", "seventy ", "eighty ",
+            "ninety " };
+
+
+    static String powerToWord(int n)
+    {
+        if (n<0)
+        {
+            return "minus"+powerToWord(-n);
+        }
+        if(n<20)
+        {
+            return oneMultiple[n];
+        }
+
+        return powerToWord(n);
+    }
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println(" Enter digit :");
+        System.out.print(" Enter digit : ");
 
-        long number = sc.nextLong();
+        int number = sc.nextInt();
 
-        System.out.println("Digit in word : ");
-        System.out.print(number);
+        System.out.print(" Digit in word : ");
+        System.out.println(powerToWord(number));
     }
 }
